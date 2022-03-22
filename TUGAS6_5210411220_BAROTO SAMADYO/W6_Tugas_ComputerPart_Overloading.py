f26c97bbebac4541343ade7b1003168626db2b11
BAROTO-SAMADYO <99570770+BAROTO-SAMADYO@users.noreply.github.com>
2022-03-22 20:22:42 +0700

    Update W6_Tugas_ComputerPart_Overloading.py

--- TUGAS6_5210411220_BAROTO SAMADYO/W6_Tugas_ComputerPart_Overloading.py
+++ TUGAS6_5210411220_BAROTO SAMADYO/W6_Tugas_ComputerPart_Overloading.py
@@ -1,12 +1,11 @@
-#FEBRIYAN BIOPSA MINANDA
-#5210411203
+
 
 #Implementasi Overloading Method pada class Computer Part
 def rupiah(uang) :
     x = str(uang)
     if len(x) <= 3 :
         return "Rp." + x
-    else :  #5210411203_Febriyan
+    else :  
         a = x[:-3]
         b = x[-3:]
         return rupiah(a) + '.' + b
@@ -14,7 +13,7 @@ def rupiah(uang) :
 class ComputerPart:
     def __init__(self, pabrikan, nama, harga) :
         self.pabrikan = pabrikan
-        self.nama = nama    #5210411203_Febriyan
+        self.nama = nama    
         self.harga = harga
 
 class Processor(ComputerPart) :
@@ -23,7 +22,7 @@ class Processor(ComputerPart) :
         self.nama = nama 
         self.harga = harga
             
-    def Tampil(self) :  #5210411203_Febriyan
+    def Tampil(self) :  
         print("\nTampil dari sub class Processor")
         print(f"{self.nama} produk dari {self.pabrikan}")
         print(f"Harga\t\t: {rupiah(self.harga)}")
@@ -31,11 +30,11 @@ class Processor(ComputerPart) :
 class RandomAccessMemory(ComputerPart) :
     def __init__(self, pabrikan, nama, harga) :
         self.pabrikan = pabrikan
-        self.nama = nama    #5210411203_Febriyan
+        self.nama = nama    
         self.harga = harga
 
     #Overload Method
-    def Tampil(self, kapasitas) :  #5210411203_Febriyan
+    def Tampil(self, kapasitas) :  
         print("\nTampil dari sub class RandomAccessMemory")
         print(f"{self.nama} produk dari {self.pabrikan}\nKapasitas\t: {kapasitas}")
         print(f"Harga\t\t: {rupiah(self.harga)}")
@@ -43,23 +42,23 @@ class RandomAccessMemory(ComputerPart) :
 class HardDiskSATA(ComputerPart) :
     def __init__(self, pabrikan, nama, harga) :
         self.pabrikan = pabrikan
-        self.nama = nama    #5210411203_Febriyan
+        self.nama = nama    
         self.harga = harga
 
     #Overload Method
-    def Tampil(self, kapasitas, rpm) :  #5210411203_Febriyan
+    def Tampil(self, kapasitas, rpm) :  
         print("\nTampil dari sub class HardDiskSATA")
         print(f"{self.nama} produk dari {self.pabrikan}\nKapasitas\t: {kapasitas}")
         print(f"RPM\t\t: {rpm}\nHarga\t\t: {rupiah(self.harga)}")
 
-#5210411203_Febriyan
+
 p = Processor('Intel', 'Core i7 7740X', 4350000)
 ram = RandomAccessMemory('V-Gen', 'DDR4 SODimm PC19200/2400MHz', 328000)
 hdd = HardDiskSATA('Seagate', 'HDD 2.5 inch', 295000)
 
 print("\n\t\t\tOVERLOADING COMPUTER PART")
 print("=================================================================================")
-p.Tampil()#5210411203_Febriyan
+p.Tampil()
 ram.Tampil("4GB")
-hdd.Tampil("500GB", 7200)#5210411203_Febriyan
+hdd.Tampil("500GB", 7200)
 print("=================================================================================\n")

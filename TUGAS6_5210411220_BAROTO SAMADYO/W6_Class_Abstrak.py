7d29263d189cc47c25bb8dd982ee3ec5de8faabf
BAROTO-SAMADYO <99570770+BAROTO-SAMADYO@users.noreply.github.com>
2022-03-22 20:21:30 +0700

    Update W6_Class_Abstrak.py

--- TUGAS6_5210411220_BAROTO SAMADYO/W6_Class_Abstrak.py
+++ TUGAS6_5210411220_BAROTO SAMADYO/W6_Class_Abstrak.py
@@ -1,5 +1,4 @@
-#FEBRIYAN BIOPSA MINANDA
-#5210411203
+
 
 #Class Abstrak
 from abc import ABC, abstractmethod
@@ -7,7 +6,7 @@ class Bentuk(ABC) :
     @abstractmethod
     def luas(self) :
         return self._sisi * self._sisi
-#5210411203_Febriyan
+
     @abstractmethod
     def keliling(self) :
         return 4 * self._sisi
@@ -15,11 +14,11 @@ class Bentuk(ABC) :
 class Persegi(Bentuk) :
     def __init__(self, sisi) :
         self._sisi = sisi
-    def luas(self) :#5210411203_Febriyan
+    def luas(self) :
         return self._sisi * self._sisi
     def keliling(self) :
         return 4 * self._sisi 
-#5210411203_Febriyan
+
 persegi = Persegi(6)
 print(persegi.luas())
 print(persegi.keliling())

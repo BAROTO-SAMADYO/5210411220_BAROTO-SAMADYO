b18e5efe9a64b077186d4b7ab9e4b07a1ce436c0
BAROTO-SAMADYO <99570770+BAROTO-SAMADYO@users.noreply.github.com>
2022-03-22 20:21:17 +0700

    Update W6_ClassSegiempat_Overriding.py

--- TUGAS6_5210411220_BAROTO SAMADYO/W6_ClassSegiempat_Overriding.py
+++ TUGAS6_5210411220_BAROTO SAMADYO/W6_ClassSegiempat_Overriding.py
@@ -1,12 +1,11 @@
-#FEBRIYAN BIOPSA MINANDA
-#5210411203
+
 
 #Implementasi Overriding Class Segi Empat
 class Segiempat() :
     def __init__(self, panjang, lebar) :
         self.panjang = panjang
         self.lebar = lebar
-    #5210411203_Febriyan
+
     def hitungLuas(self) :
         print(f"Luas Segiempat : {self.panjang * self.lebar}")
 
@@ -14,7 +13,7 @@ class Bujursangkar(Segiempat) :
     def __init__(self,sisi) :
         self.side = sisi
         Segiempat.__init__(self, sisi, sisi)
-    #5210411203_Febriyan
+
 
     #Overriding
     def hitungLuas(self) :
@@ -22,5 +21,5 @@ class Bujursangkar(Segiempat) :
 
 b = Bujursangkar(4)
 s = Segiempat(2, 4)
-b.hitungLuas()#5210411203_Febriyan
+b.hitungLuas()
 s.hitungLuas()

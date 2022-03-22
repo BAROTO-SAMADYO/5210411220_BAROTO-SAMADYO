4547ef6af7d2d6b159d54fefe0aac93f56f486d4
BAROTO-SAMADYO <99570770+BAROTO-SAMADYO@users.noreply.github.com>
2022-03-22 20:23:02 +0700

    Update W6_Tugas_ComputerPart_Overriding.py

--- TUGAS6_5210411220_BAROTO SAMADYO/W6_Tugas_ComputerPart_Overriding.py
+++ TUGAS6_5210411220_BAROTO SAMADYO/W6_Tugas_ComputerPart_Overriding.py
@@ -1,12 +1,11 @@
-#FEBRIYAN BIOPSA MINANDA
-#5210411203
+
 
 #Implementasi Overriding Method pada class Computer Part
 def rupiah(uang) :
     x = str(uang)
     if len(x) <= 3 :
         return "Rp." + x
-    else :  #5210411203_Febriyan
+    else :  
         a = x[:-3]
         b = x[-3:]
         return rupiah(a) + '.' + b
@@ -14,7 +13,7 @@ def rupiah(uang) :
 class ComputerPart:
     def __init__(self, pabrikan, nama, harga) :
         self.pabrikan = pabrikan
-        self.nama = nama    #5210411203_Febriyan
+        self.nama = nama    
         self.harga = harga
 
 class Processor(ComputerPart) :
@@ -23,7 +22,7 @@ class Processor(ComputerPart) :
         self.nama = nama 
         self.harga = harga
         
-    def Tampil(self) :  #5210411203_Febriyan
+    def Tampil(self) :  
         print("\nTampil dari sub class Processor")
         print(f"{self.nama} produk dari {self.pabrikan}")
         print(f"Harga\t\t: {rupiah(self.harga)}")
@@ -34,7 +33,7 @@ class RandomAccessMemory(ComputerPart) :
         self.kapasitas = kapasitas
 
     # Override  Method
-    def Tampil(self) :  #5210411203_Febriyan
+    def Tampil(self) :  
         print("Tampil dari sub class RandomAccessMemory")
         print(f"{self.nama} produk dari {self.pabrikan}\nKapasitas\t: {self.kapasitas}")
         print(f"Harga\t\t: {rupiah(self.harga)}")
@@ -50,15 +49,15 @@ class HardDiskSATA(ComputerPart) :
         print("Tampil dari sub class HardDiskSATA")
         print(f"{self.nama} produk dari {self.pabrikan}\nKapasitas\t: {self.kapasitas}")
         print(f"RPM\t\t: {self.rpm}\nHarga\t\t: {rupiah(self.harga)}")
-#5210411203_Febriyan
+
 p = Processor('Intel', 'Core i7 7740X', 4350000)
 ram = RandomAccessMemory('V-Gen', 'DDR4 SODimm PC19200/2400MHz', 328000, "4GB")
 hdd = HardDiskSATA('Seagate', 'HDD 2.5 inch', 295000, '500GB', 7200)
 
-parts = [p, ram, hdd]   #5210411203_Febriyan
+parts = [p, ram, hdd]   
 print("\n\t\t\tOVERRIDING COMPUTER PART")
 print("=================================================================================")
 for part in parts :
     part.Tampil()
-    print("")   #5210411203_Febriyan
+    print("")   
 print("=================================================================================\n")
